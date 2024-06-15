06a21aee1ac1b610f406593330764f9033c09157
farikoh01 <91357376+farikoh01@users.noreply.github.com>
2024-06-16 02:52:45 +0700

    Update hotels.js

--- src/components/hotels.js
+++ src/components/hotels.js
@@ -1,9 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Pastikan Bootstrap CSS diimpor
 // import React from "react";
 // import pandawaBeach from "../assets/img/homescreen/pandawa-beach.png";
-import React, { useState } from "react";
-import hotel1 from "../assets/img/hotel/hotel-1.jpeg";
+import hotel1 from "../assets/img/hotels/hotel-1.jpeg";
 import "../css/hotels.css"; // Pastikan ini mengarah ke file CSS yang benar
+import React, { useState } from "react";
 
 const Hotels = () => {
   const [priceRange, setPriceRange] = useState([0, 10000000]);
@@ -12,37 +12,37 @@ const Hotels = () => {
     facilities: [],
   });
 
-  const handlePriceChange = (e) => {
-    setPriceRange([0, parseInt(e.target.value)]);
-  };
+  // const handlePriceChange = (e) => {
+  //   setPriceRange([0, parseInt(e.target.value)]);
+  // };
 
-  const handleFilterChange = (e) => {
-    const { name, value, checked } = e.target;
-    setFilters((prevFilters) => {
-      if (checked) {
-        return {
-          ...prevFilters,
-          [name]: [...prevFilters[name], value],
-        };
-      } else {
-        return {
-          ...prevFilters,
-          [name]: prevFilters[name].filter((filter) => filter !== value),
-        };
-      }
-    });
-  };
+  // const handleFilterChange = (e) => {
+  //   const { name, value, checked } = e.target;
+  //   setFilters((prevFilters) => {
+  //     if (checked) {
+  //       return {
+  //         ...prevFilters,
+  //         [name]: [...prevFilters[name], value],
+  //       };
+  //     } else {
+  //       return {
+  //         ...prevFilters,
+  //         [name]: prevFilters[name].filter((filter) => filter !== value),
+  //       };
+  //     }
+  //   });
+  // };
 
   return (
     <div className="home-screen">
-      <header className="header">
+      <header className="header fixed-top">
         <div className="logo">
           <span>
             DESTINA<span>6</span>
           </span>
         </div>
         <nav className="nav-bar">
-          <a href="/">Home</a>
+          <a href="homescreen">Home</a>
           <a href="/faq">FAQ</a>
           <a href="/contact">Contact</a>
           <a href="/signin">Sign in</a>
@@ -52,8 +52,10 @@ const Hotels = () => {
       <main className="container mt-4">
         <div className="tab-navigation ">
           <a href="/destination">Destinations</a>
-          <a href="/hotels">Hotels</a>
-          <a href="/flights">Flight</a>
+          <a href="/hotel" className="hotel">
+            Hotels
+          </a>
+          <a href="/flight">Flight</a>
           <a href="/bus">Bus</a>
         </div>
 
@@ -66,81 +68,104 @@ const Hotels = () => {
         </div>
 
         <div className="row">
-          <div className="col-md-3">
-            <div className="filter-section">
+          <div className="col-md-3 ">
+            <div className="filter-section ">
               <div className="price">
-                <p>
+                <h5>
                   <b>
-                    <center>Price (Rp 0 - Rp 10,000,000+)</center>
+                    Price (Rp {priceRange[0].toLocaleString()} - Rp {priceRange[1].toLocaleString()})
                   </b>
-                </p>
-                <input type="range" min="0" max="10000000" step="50000" value={priceRange[1]} onChange={handlePriceChange} className="form-range" />
-                <p>
-                  <center>Up to: Rp {priceRange[1].toLocaleString()}</center>
-                </p>
-                <div className="price-options">
-                  <label>
-                    <input type="button" className="btn btn-secondary mb-2" name="price" value="0-300000" onChange={handlePriceChange} />
-                  </label>
-                  <br />
-                  <label>
-                    <input type="button" className="btn btn-secondary mb-2" name="price" value="0-5000000" onChange={handlePriceChange} />
-                  </label>
-                  <br />
-                  <label>
-                    <input type="button" className="btn btn-secondary" name="price" value="0-10000000" onChange={handlePriceChange} />
-                  </label>
+                </h5>
+                <input type="range" min="0" max="10000000" step="100000" value={priceRange[1]} onChange={(e) => setPriceRange([priceRange[0], e.target.value])} className="form-range" />
+                <div className="price-options d-flex flex-column">
+                  <button className="btn btn-dark mb-1" onClick={() => setPriceRange([0, 300000])}>
+                    Rp 0 - Rp 300,000
+                  </button>
+                  <button className="btn btn-dark mb-1" onClick={() => setPriceRange([0, 5000000])}>
+                    Rp 0 - Rp 5,000,000
+                  </button>
+                  <button className="btn btn-dark mb-1" onClick={() => setPriceRange([0, 10000000])}>
+                    Rp 0 - Rp 10,000,000
+                  </button>
                 </div>
               </div>
             </div>
 
             <div className="filter-section mt-2">
               <div className="popular">
-                <h4>Popular Filters</h4>
-                <label>
-                  <input type="checkbox" name="popular" value="breakfast" onChange={handleFilterChange} /> Breakfast Included
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="popular" value="payment" onChange={handleFilterChange} /> Only Payment
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="popular" value="payAtHotel" onChange={handleFilterChange} /> Pay at Hotel
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="popular" value="singleBed" onChange={handleFilterChange} /> Single Bed
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="popular" value="doubleBed" onChange={handleFilterChange} /> Double Bed
-                </label>
+                <h5>
+                  <b>Popular Filters</b>
+                </h5>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany1" />
+                  <label className="form-check-label" htmlFor="busCompany1">
+                    Breakfast Included
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Only Payment
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Pay at Hotel
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Single Bed
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Double Bed
+                  </label>
+                </div>
+                {/* Add more bus companies as needed */}
               </div>
             </div>
 
             <div className="filter-section mt-2">
               <div className="facilities">
-                <h4>Facilities</h4>
-                <label>
-                  <input type="checkbox" name="facilities" value="wifi" onChange={handleFilterChange} /> WiFi
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="facilities" value="pool" onChange={handleFilterChange} /> Swimming Pool
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="facilities" value="parking" onChange={handleFilterChange} /> Parking
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="facilities" value="restaurant" onChange={handleFilterChange} /> Restaurant
-                </label>
-                <br />
-                <label>
-                  <input type="checkbox" name="facilities" value="elevator" onChange={handleFilterChange} /> Elevator
-                </label>
+                <h5>
+                  <b>Facilities</b>
+                </h5>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany1" />
+                  <label className="form-check-label" htmlFor="busCompany1">
+                    WiFi
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Swimming Pool
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Parking
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Restaurant
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="busCompany2" />
+                  <label className="form-check-label" htmlFor="busCompany2">
+                    Elevator
+                  </label>
+                </div>
+                {/* Add more bus companies as needed */}
               </div>
             </div>
           </div>
@@ -154,14 +179,17 @@ const Hotels = () => {
                   </div>
                   <div className="col-md-8">
                     <div className="card-body">
-                      <h3 className="card-title">Pandawa Beach Hotel</h3>
+                      <h3 className="card-title">
+                        <b>Pandawa Beach Hotel</b>
+                      </h3>
                       <div className="d-flex justify-content-left align-items-center">
-                        <a className="btn btn-secondary">4.0/5</a>
+                        <a className="btn btn-warning">☆☆☆☆☆</a>
                         <h10 className="card-text">Very Good</h10>
                       </div>
 
-                      <div className="kota-text">
-                        <small className="text-muted">Near Kuta Beach - Kuta (show on map)</small>
+                      <div className="kota-text d-flex grid gap-2">
+                        <p className="text-muted">Near Kuta Beach - Kuta</p>
+                        <p className="map">Show on Map</p>
                         <br />
                       </div>
 
@@ -179,118 +207,12 @@ const Hotels = () => {
                         </div>
 
                         <div className="col-5 right">
-                          <a className="btn btn-danger">20% OFF</a>
-                          <p className="">
-                            <h5>
-                              <b>Price: </b> Rp 1,500,000
-                            </h5>
-                          </p>
+                          <a className="btn btn-danger mb-3">20% OFF</a>
+                          <h5 className="mb-3">
+                            <b>Price: Rp 1,500,000</b>
+                          </h5>
                           <div className="d-flex justify-content-end align-items-center">
-                            <a href="/payment" type="button" className="btn btn-primary btn-sm">
-                              Book Now
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* Add more hotel cards as needed */}
-            </div>
-              <div className="hotel-list">
-              <div className="hotel-card card mb-4">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <img src={hotel1} className="img-fluid rounded-start" alt="Hotel" />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h3 className="card-title">Pandawa Beach Hotel</h3>
-                      <div className="d-flex justify-content-left align-items-center">
-                        <a className="btn btn-secondary">4.0/5</a>
-                        <h10 className="card-text">Very Good</h10>
-                      </div>
-
-                      <div className="kota-text">
-                        <small className="text-muted">Near Kuta Beach - Kuta (show on map)</small>
-                        <br />
-                      </div>
-
-                      <div className="row room-info p-2 mb-2">
-                        <div className="col-7 left">
-                          <div>
-                            <h10>Type Room : Standard Room</h10>
-                          </div>
-                          <div>
-                            <h10>Benefit : Breakfast Included</h10>
-                          </div>
-                          <div className="red">
-                            <h10>Facility : Only 1 room left, bathroom</h10>
-                          </div>
-                        </div>
-
-                        <div className="col-5 right">
-                          <a className="btn btn-danger">20% OFF</a>
-                          <p className="">
-                            <h5>
-                              <b>Price: </b> Rp 1,500,000
-                            </h5>
-                          </p>
-                          <div className="d-flex justify-content-end align-items-center">
-                            <a href="/payment" type="button" className="btn btn-primary btn-sm">
-                              Book Now
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* Add more hotel cards as needed */}
-            </div>
-              <div className="hotel-list">
-              <div className="hotel-card card mb-4">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <img src={hotel1} className="img-fluid rounded-start" alt="Hotel" />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h3 className="card-title">Pandawa Beach Hotel</h3>
-                      <div className="d-flex justify-content-left align-items-center">
-                        <a className="btn btn-secondary">4.0/5</a>
-                        <h10 className="card-text">Very Good</h10>
-                      </div>
-
-                      <div className="kota-text">
-                        <small className="text-muted">Near Kuta Beach - Kuta (show on map)</small>
-                        <br />
-                      </div>
-
-                      <div className="row room-info p-2 mb-2">
-                        <div className="col-7 left">
-                          <div>
-                            <h10>Type Room : Standard Room</h10>
-                          </div>
-                          <div>
-                            <h10>Benefit : Breakfast Included</h10>
-                          </div>
-                          <div className="red">
-                            <h10>Facility : Only 1 room left, bathroom</h10>
-                          </div>
-                        </div>
-
-                        <div className="col-5 right">
-                          <a className="btn btn-danger">20% OFF</a>
-                          <p className="">
-                            <h5>
-                              <b>Price: </b> Rp 1,500,000
-                            </h5>
-                          </p>
-                          <div className="d-flex justify-content-end align-items-center">
-                            <a href="/payment" type="button" className="btn btn-primary btn-sm">
+                            <a href="/signin" className="btn btn-primary ">
                               Book Now
                             </a>
                           </div>
